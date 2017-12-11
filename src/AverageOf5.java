6f9d3162862c491e16883565b33483178fc5766b
Jones <Owain.Jones@tdsb.on.ca>
2017-12-11 12:28:16 -0500

    Initial commit

---
+++ src/AverageOf5.java
@@ -0,0 +1,19 @@
+import io.Input;
+import io.Output;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AverageOf5 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        /* write your code here */
+
+    }
+
+
+    /* Leave this method */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
